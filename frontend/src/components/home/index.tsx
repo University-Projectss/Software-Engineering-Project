51b35a5d3321690ff53094420fa5662c573562ec
Robert Udrea <robert.udrea@cst.ro>
2023-11-09 21:29:33 +0200

    micrate to next + tailwind

--- frontend/src/components/home/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Text } from "@chakra-ui/react";
-
-export const Home = () => {
-  return (
-    <Flex direction="column" height="100vh" align="center" justify="center">
-      <Text fontFamily="Poppins">Hello there</Text>
-    </Flex>
-  );
-};
