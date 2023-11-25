1f9347c5d81e29c184031a16efcf6f582f5c1c03
Qmpzlawasd <diaconustefan16@gmail.com>
2023-11-26 00:52:46 +0200

    Add ticket related models

---
+++ backend/src/main/java/ro/boa/clinic/model/Ticket.java
@@ -0,0 +1,33 @@
+package ro.boa.clinic.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tickets")
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private Doctor doctor;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "specialization")
+    private String specialization;
+}
