1726911ccd130caa6e696a57414669083963e68b
AdelaCorbeanu <92207857+AdelaCorbeanu@users.noreply.github.com>
2023-12-06 16:04:11 +0200

    Tickets dashboard (#36)
    * Tickets dashboard
    
    * Small updates for TicketInterface

---
+++ frontend/src/components/home/Ticket.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import {
+    Box,
+    Text,
+    Divider,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    IconButton,
+    Badge,
+} from "@chakra-ui/react";
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+import { FaTrash, FaPen } from "react-icons/fa";
+import { colors } from "../../theme";
+import { TicketInterface } from "./types";
+
+interface TicketProps {
+    ticket: TicketInterface
+}
+
+export const Ticket: React.FC<TicketProps> = ({
+    ticket
+}) => {
+    return (
+        <Box
+            bgColor="white"
+            p={4}
+            borderRadius="xl"
+            boxShadow="md"
+            w="300px"
+            position="relative"
+        >
+            {/* 3-dots menu */}
+            <Menu>
+                <MenuButton
+                    as={IconButton}
+                    icon={<BiDotsHorizontalRounded />}
+                    size="lg"
+                    variant="ghost"
+                    aria-label="Options"
+                    position="absolute"
+                    top="0"
+                    right="0"
+                />
+                <MenuList>
+                    <MenuItem icon={<FaPen />} onClick={() => console.log("Edit")}>
+                        Edit
+                    </MenuItem>
+                    <MenuItem
+                        icon={<FaTrash />}
+                        color="red.500"
+                        onClick={() => console.log("Delete")}
+                    >
+                        Delete
+                    </MenuItem>
+                </MenuList>
+            </Menu>
+
+            {/* Ticket content */}
+            <Box>
+                <Text fontSize="lg" fontWeight="bold" color="black">
+                    Dr. {ticket.doctorName}
+                </Text>
+                <Text fontSize="md" color="black">
+                    Date:{" "}
+                    <Text as="span" fontWeight="bold">
+                        {ticket.date}
+                    </Text>
+                    {/* Specialization Badge */}
+                    <Badge ml={2} bg={colors.blue} color="white">
+                        {ticket.specialization}
+                    </Badge>
+                </Text>
+                <Divider borderColor="black" mt={2} mb={2} />
+                <Text
+                    fontSize="md"
+                    color="black"
+                    mt={4}
+                    fontWeight="bold"
+                    noOfLines={3}
+                >
+                    Your Request
+                </Text>
+                <Text fontSize="md" color="black" noOfLines={3}>
+                    {ticket.userRequest}
+                </Text>
+            </Box>
+
+            <Box textAlign="center">
+                <Button
+                    colorScheme="gray"
+                    variant="solid"
+                    fontWeight="bold"
+                    mt={4}
+                    bgColor="#C0C0C0"
+                >
+                    See more details
+                </Button>
+            </Box>
+        </Box>
+    );
+};
---
+++ frontend/src/components/home/TicketsTabContent.tsx
@@ -0,0 +1,36 @@
+// TicketsTabContent.tsx
+import React from "react";
+import { Box, Flex, TabPanel, Text } from "@chakra-ui/react";
+import { Ticket } from "./Ticket";
+import { TicketInterface } from "./types";
+
+interface TicketsTabContentProps {
+    tickets: TicketInterface[];
+    text: string;
+}
+
+export const TicketsTabContent: React.FC<TicketsTabContentProps> = ({
+    tickets,
+    text,
+}) => {
+    return (
+        <TabPanel>
+            <Text fontSize="4xl" fontWeight="bold" color="black" pl={10}>
+                {text}
+            </Text>
+            <Flex mt={4} pl={10}>
+                {tickets.length > 0 ? (
+                    tickets.map((ticket) => (
+                        <Box mr={4}>
+                            <Ticket
+                                ticket={ticket}                                
+                            />
+                        </Box>
+                    ))
+                ) : (
+                    <Text>{`No ${text.toLowerCase()} tickets`}</Text>
+                )}
+            </Flex>
+        </TabPanel>
+    );
+};
