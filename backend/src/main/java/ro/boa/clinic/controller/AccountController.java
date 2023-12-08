c125541a2654190044646cf537073480bb2a3a57
tezkerek <andrei.ancuta@gmail.com>
2023-12-08 16:34:30 +0200

    Add account registration endpoint

---
+++ backend/src/main/java/ro/boa/clinic/controller/AccountController.java
@@ -0,0 +1,27 @@
+package ro.boa.clinic.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ro.boa.clinic.dto.AccountRegistrationDto;
+import ro.boa.clinic.service.AccountService;
+
+@RequiredArgsConstructor
+@RestController
+public class AccountController {
+    private final AccountService accountService;
+
+    @PostMapping("/accounts")
+    public ResponseEntity<Void> createAccount(@RequestBody AccountRegistrationDto accountRegistrationDto) {
+        try {
+            accountService.createAccount(accountRegistrationDto.email(), accountRegistrationDto.password());
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
