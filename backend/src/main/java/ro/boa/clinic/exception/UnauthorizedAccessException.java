993631e14088da94bf7c34470161455d80634a9f
irinaenescu2002 <irinaenescu2002@gmail.com>
2023-12-22 19:48:56 +0200

    Update unauthorized access message

--- backend/src/main/java/ro/boa/clinic/exception/UnauthorizedAccessException.java
+++ backend/src/main/java/ro/boa/clinic/exception/UnauthorizedAccessException.java
@@ -3,6 +3,6 @@ package ro.boa.clinic.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "The logged-in user do not have access to this resource")
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "The logged-in user does not have access to this resource")
 public class UnauthorizedAccessException extends RuntimeException{
 }
