ec711d67b03c792571f2c4561f85984a1ae135d1
AdelaCorbeanu <92207857+AdelaCorbeanu@users.noreply.github.com>
2023-12-24 10:25:47 +0200

    Add unit tests for patient tickets dashboard (#49)
    * Add unit tests
    
    * Modify .gitignore
    
    * Ignore tracked files

---
+++ frontend/src/components/home/unitTests/Ticket.test.tsx
@@ -0,0 +1,63 @@
+// Ticket.test.tsx
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Ticket } from '../Ticket';
+import { TicketInterface } from '../types';
+
+const mockTicket: TicketInterface = {
+    doctorName: 'John Doe',
+    date: '2023-01-01',
+    userRequest: 'Sample request',
+    status: 'opened',
+    specialization: 'Cardiology',
+};
+
+describe('Ticket Component', () => {
+    it('renders ticket content correctly', () => {
+        render(<Ticket ticket={mockTicket} />);
+
+        // Check if the doctor's name is rendered
+        expect(screen.getByText(/Dr\. John Doe/i)).toBeInTheDocument();
+
+        // Check if the date is rendered
+        expect(screen.getByText(/Date:/i)).toBeInTheDocument();
+        expect(screen.getByText(/2023-01-01/i)).toBeInTheDocument();
+
+        // Check if the specialization badge is rendered
+        expect(screen.getByText(/Cardiology/i)).toBeInTheDocument();
+
+        // Check if the user request is rendered
+        expect(screen.getByText(/Your Request/i)).toBeInTheDocument();
+        expect(screen.getByText(/Sample request/i)).toBeInTheDocument();
+
+        // Check if the "See more details" button is rendered
+        expect(screen.getByText(/See more details/i)).toBeInTheDocument();
+    });
+
+    it('handles 3-dots-menu click', async () => {
+        render(<Ticket ticket={mockTicket} />);
+
+        // Ensure menu is initially closed
+        const editMenuItem = screen.queryByText(/Edit/i);
+        const deleteMenuItem = screen.queryByText(/Delete/i);
+
+        expect(editMenuItem).not.toBeVisible();
+        expect(deleteMenuItem).not.toBeVisible();
+
+        // Click the 3-dots-menu button
+        fireEvent.click(screen.getByLabelText('Options'));
+
+        // Wait for the "Edit" menu item to become visible
+        await waitFor(() => {
+            const updatedEditMenuItem = screen.queryByText(/Edit/i);
+            expect(updatedEditMenuItem).toBeVisible();
+        });
+
+        // Wait for the "Delete" menu item to become visible
+        await waitFor(() => {
+            const updatedDeleteMenuItem = screen.queryByText(/Delete/i);
+            expect(updatedDeleteMenuItem).toBeVisible();
+        });
+    });
+});
