c39a661535bca4897e0edb82e3fa82070f1605c0
Irina Enescu <93870739+irinaenescu2002@users.noreply.github.com>
2024-01-08 01:22:06 +0200

    Add more data to seeder (#86)

--- backend/src/main/java/ro/boa/clinic/runner/Seeder.java
+++ backend/src/main/java/ro/boa/clinic/runner/Seeder.java
@@ -4,52 +4,56 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import ro.boa.clinic.dto.TicketCreationRequestDto;
 import ro.boa.clinic.model.Sex;
-import ro.boa.clinic.service.AccountService;
-import ro.boa.clinic.service.DoctorService;
-import ro.boa.clinic.service.PatientService;
-import ro.boa.clinic.service.TicketService;
 
 import java.time.LocalDate;
 
 @RequiredArgsConstructor
 @Component
 public class Seeder implements ApplicationRunner {
-    private final AccountService accountService;
-    private final PatientService patientService;
-    private final DoctorService doctorService;
-    private final TicketService ticketService;
+    private final SeederHelper seederHelper;
 
     @Override
     public void run(ApplicationArguments args) {
-        accountService.createPatientAccount("user1@example.com", "Password1");
+        var emmaJohnson = seederHelper.createPatientAccountAndProfile(
+                "Emma", "Johnson", Sex.FEMALE, LocalDate.of(1985, 1, 15));
+        var ethanSmith = seederHelper.createPatientAccountAndProfile(
+                "Ethan Liam", "Smith", Sex.MALE, LocalDate.of(1992, 5, 18));
+        var oliviaWilliams = seederHelper.createPatientAccountAndProfile(
+                "Olivia", "Williams", Sex.FEMALE, LocalDate.of(1988, 11, 30));
+        var noahDavid = seederHelper.createPatientAccountAndProfile(
+                "Noah John", "Davis", Sex.MALE, LocalDate.of(2000, 6, 8));
 
-        var account1 = accountService.createPatientAccount("user2@example.com", "Password2");
-        var patient1 = patientService.createPatientProfile("John", "Doe", Sex.MALE, LocalDate.of(2004, 1, 8), account1.getEmail());
+        seederHelper.createDoctorAccountAndProfile("Sarah", "Anderson", "Cardiology");
+        seederHelper.createDoctorAccountAndProfile("Michael", "Roberts", "Pediatrics");
+        seederHelper.createDoctorAccountAndProfile("Emily", "Davis", "Neurology");
+        seederHelper.createDoctorAccountAndProfile("Natalie", "Brown", "Cardiology");
+        seederHelper.createDoctorAccountAndProfile("James", "Miller", "Orthopedic Surgery");
+        seederHelper.createDoctorAccountAndProfile("Lauren", "Taylor", "Dermatologist");
+        seederHelper.createDoctorAccountAndProfile("Benjamin", "Carter", "Psychiatry");
+        seederHelper.createDoctorAccountAndProfile("Sophia", "Lewis", "Obstetrics/Gynecology");
+        seederHelper.createDoctorAccountAndProfile("David", "Martinez", "Cardiology");
+        seederHelper.createDoctorAccountAndProfile("Ryan", "Anderson", "Neurology");
+        seederHelper.createDoctorAccountAndProfile("Justin", "Johnson", "Dermatology");
+        seederHelper.createDoctorAccountAndProfile("Alex", "Turner", "Gastroenterology");
 
-        var account2 = accountService.createPatientAccount("user3@example.com", "Password3");
-        var patient2 = patientService.createPatientProfile("Clara", "Doe", Sex.FEMALE, LocalDate.of(2004, 1, 8), account2.getEmail());
-
-        var account3 = accountService.createDoctorAccount("user4@example.com", "Password4");
-        var doctor1 = doctorService.createDoctorProfile("Lara", "Doe", "Gastroenterologie", account3.getEmail());
-
-        var account4 = accountService.createDoctorAccount("user5@example.com", "Password5");
-        var doctor2 = doctorService.createDoctorProfile("Jonah", "Doe", "Neurologie", account4.getEmail());
-
-        var account5 = accountService.createDoctorAccount("user7@example.com", "Password7");
-        var doctor3 = doctorService.createDoctorProfile("Lisa", "Doe", "Stomatologie", account5.getEmail());
-
-        var ticketToCreate1 = new TicketCreationRequestDto("Durere de burta",
-                "Ma doare burta cand mananc cartofi prajiti", "Gastroenterologie");
-        ticketService.createTicket(ticketToCreate1, patient1);
-
-        var ticketToCreate2 = new TicketCreationRequestDto("Durere de cap",
-                "Ma doare capul cand ma enervez", "Neurologie");
-        ticketService.createTicket(ticketToCreate2, patient2);
-
-        var ticketToCreate3 = new TicketCreationRequestDto("Durere de masea",
-                "Ma doare maseaua cand beau apa rece", "Stomatologie");
-        ticketService.createTicket(ticketToCreate3, patient2);
+        seederHelper.createTicket(oliviaWilliams, "Chest Pain",
+                "I experience sharp chest pain after physical activity", "Cardiology");
+        seederHelper.createTicket(emmaJohnson, "Child's Persistent Cough",
+                "My child has been coughing persistently for a week", "Pediatrics");
+        seederHelper.createTicket(emmaJohnson, "Chronic Headaches",
+                "I suffer from chronic headaches and migraines", "Neurology");
+        seederHelper.createTicket(emmaJohnson, "Joint Pain and Stiffness",
+                "I have been experiencing joint pain and stiffness in my knees", "Orthopedic Surgery");
+        seederHelper.createTicket(oliviaWilliams, "Acne Troubles",
+                "I've been struggling with persistent acne breakouts", "Dermatology");
+        seederHelper.createTicket(noahDavid, "Skin Rash",
+                "I have developed a rash on my arms and need assistance", "Dermatology");
+        seederHelper.createTicket(oliviaWilliams, "Menstrual Irregularities",
+                "I am experiencing irregularities in my menstrual cycle", "Obstetrics/Gynecology");
+        seederHelper.createTicket(ethanSmith, "Anxiety and Sleep Issues",
+                "I am struggling with anxiety and facing difficulties with sleep", "Psychiatry");
+        seederHelper.createTicket(ethanSmith, "Digestive Discomfort",
+                "I am experiencing discomfort in my stomach after eating fried potatoes", "Gastroenterology");
     }
 }
